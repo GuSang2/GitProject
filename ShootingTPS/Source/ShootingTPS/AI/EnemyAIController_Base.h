4ac35c12224baa7c634a9fc5acbd1d9ea4c1cd3b
GuSang2 <lsg0101231@naver.com>
2025-04-08 10:21:57 +0900

    프로젝트 재생성

---
+++ ShootingTPS/Source/ShootingTPS/AI/EnemyAIController_Base.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "EnemyAIController_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTINGTPS_API AEnemyAIController_Base : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+	void StopBehaviorTree();
+
+protected:
+	virtual void OnPossess(APawn* InPawn) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "AI")
+	class UBehaviorTree* BehaviorTree;
+};
