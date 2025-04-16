3f26725d706e2cf8568ba6219e279d6c6a0b0245
GuSang2 <lsg0101231@naver.com>
2025-04-16 17:24:27 +0900

    AI 인식 수정 완료

--- ShootingTPS/Source/ShootingTPS/AI/EnemyAIController_Base.h
+++ ShootingTPS/Source/ShootingTPS/AI/EnemyAIController_Base.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
-#include "Perception/AIPerceptionTypes.h"
 #include "EnemyAIController_Base.generated.h"
 
 /**
@@ -23,16 +22,7 @@ public:
 protected:
 	virtual void OnPossess(APawn* InPawn) override;
 
-	UFUNCTION()
-	void OnTargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
-
 protected:
 	UPROPERTY(EditAnywhere, Category = "AI")
 	class UBehaviorTree* BehaviorTree;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
-	class UAIPerceptionComponent* AIPerception = nullptr;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
-	class UAISenseConfig_Sight* SightConfig = nullptr;
 };
